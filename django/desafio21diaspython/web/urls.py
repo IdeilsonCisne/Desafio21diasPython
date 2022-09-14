1a6ec5bc845f6411c35224c1c37c87c930c8105b
Ideilson Cisne <deilson.cisne@gmail.com>
2022-09-13 23:19:43 -0300

    dia 18

--- django/desafio21diaspython/web/urls.py
+++ django/desafio21diaspython/web/urls.py
@@ -1,7 +1,15 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-  path('', views.index)
-]
+  path('', views.index),
+  path('about', views.about),
+  path('contact', views.contact),
+  path('post', views.post),
+  path('json', views.json),
+  path('html_bruto', views.html_bruto),
+  path('xml_bruto', views.xml_bruto),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- django/desafio21diaspython/web/views.py
+++ django/desafio21diaspython/web/views.py
@@ -1,7 +1,31 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Olá turma do desafio 21 dias de python")
+  resposta = {}
+  resposta["conteudo"] = "Agora com conteúdo vindo do controller"
+  return render(request, 'home/index.html', resposta)
+
+def about(request):
+  return render(request, 'home/about.html')
+
+def contact(request):
+  return render(request, 'home/contact.html')
+
+def post(request):
+  return render(request, 'home/post.html')
+
+def html_bruto(request):
+  return HttpResponse("<h1>Respondendo com html</h1>")
+
+def json(request):
+  resposta = {}
+  resposta["conteudo"] = "Agora com API JSON"
+  return JsonResponse(resposta)
+
+
+def xml_bruto(request):
+  xml_bruto = '<?xml version="1.0" encoding="UTF-8"?><bookstore><book category="cooking"><title lang="en">Everyday Italian</title><author>Giada De Laurentiis</author><year>2005</year><price>30.00</price></book></bookstore>'
+  return HttpResponse(xml_bruto, content_type="text/xml")
